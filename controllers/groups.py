86d4c58925be4144bc7f199fbbf95648976d27a7
Catherine Brett <catherinejanebrett@icloud.com>
2024-04-15 16:42:58 +0100

    Adds remove method to Base model. Adds following group endpoints: get single, create, update, delete.

--- controllers/groups.py
+++ controllers/groups.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from models.group import GroupModel
+from models.category import CategoryModel
 from serializers.group import GroupSerializer
 from serializers.category import CategorySerializer
 from serializers.group_category import GroupCategorySerializer
@@ -16,3 +17,40 @@ router = Blueprint("groups", __name__)
 def get_all_groups():
     groups = GroupModel.query.all()
     return group_serializer.jsonify(groups, many=True)
+
+
+@router.route("/groups/<int:group_id>", methods=["GET"])
+def get_single_group(group_id):
+    group = GroupModel.query.get(group_id)
+    return group_serializer.jsonify(group)
+
+
+@router.route("/groups", methods=["POST"])
+def create_group():
+    group_dictionary = request.json
+    # TO-DO: update this hardcoded user ID once you've done your secure route:
+    group_dictionary["user_id"] = 1
+    # TO-DO: figure out what to do about categories
+    group = group_serializer.load(group_dictionary)
+    group.save()
+    return group_serializer.jsonify(group)
+
+
+@router.route("/groups/<int:group_id>", methods=["PUT"])
+def update_group(group_id):
+    group_dictionary = request.json
+    group_to_update = GroupModel.query.get(group_id)
+
+    group = group_serializer.load(
+        group_dictionary, instance=group_to_update, partial=True
+    )
+
+    group.save()
+    return group_serializer.jsonify(group)
+
+
+@router.route("/groups/<int:group_id>", methods=["DELETE"])
+def delete_group(group_id):
+    group = GroupModel.query.get(group_id)
+    group.remove()
+    return ""
--- models/base.py
+++ models/base.py
@@ -16,3 +16,7 @@ class BaseModel:
     def save(self):
         db.session.add(self)
         db.session.commit()
+
+    def remove(self):
+        db.session.delete(self)
+        db.session.commit()
