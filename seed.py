c5196130a4d60beb22d67fdd0368e55f27fa1100
Catherine Brett <catherinejanebrett@icloud.com>
2024-04-16 21:39:00 +0100

    Updates seed with new Italy photo and shorter brief_descs.

--- seed.py
+++ seed.py
@@ -47,7 +47,7 @@ with app.app_context():
     harborne_walking_group = GroupModel(
         name="Harborne Walking Group",
         image="https://images.unsplash.com/photo-1580058572462-98e2c0e0e2f0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2Fsa2luZyUyMGdyb3VwfGVufDB8MHwwfHx8Mg",
-        brief_desc="Easy-going walking group.",
+        brief_desc="Easy-going walking group",
         contact_name="Catherine",
         contact_number="07010 110 110",
         full_desc="Come and join our friendly walking group! Every other Sunday we meet on the high street and then do a 5k walk, finishing where we started. We often have refreshments at a cafe afterwards. All are welcome!",
@@ -58,7 +58,7 @@ with app.app_context():
     nifty_knitters = GroupModel(
         name="Nifty Knitters",
         image="https://images.unsplash.com/photo-1562469162-c17fc5155156?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8a25pdHRpbmd8ZW58MHwwfDB8fHwy",
-        brief_desc="Knitting group for all ages!",
+        brief_desc="Knitting group for all ages",
         contact_name="Wendy",
         contact_number="07020 220 220",
         full_desc="We meet once a week on a Wednesday evening for some company while we work on our individual creations, and also help each other out along the way! Call Wendy for more info.",
@@ -69,7 +69,7 @@ with app.app_context():
     harborne_daytrippers = GroupModel(
         name="Harborne Daytrippers",
         image="https://images.unsplash.com/photo-1557223562-6c77ef16210f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YnVzJTIwdG91ciUyMGZyZWUlMjB0byUyMHVzZXxlbnwwfDB8MHx8fDI",
-        brief_desc="Days out in the Midlands amongst friends!",
+        brief_desc="Days out in the Midlands",
         contact_name="George",
         contact_number="07030 330 330",
         full_desc="At Harborne Daytrippers we take it in turns to organise a day out to a Midlands attraction on the last Saturday of each month. We try to keep the cost of tickets and transport down to under £50. We look forward to meeting you!",
@@ -80,7 +80,7 @@ with app.app_context():
     birmingham_bookworms = GroupModel(
         name="Birmingham Bookworms",
         image="https://images.unsplash.com/photo-1513475382585-d06e58bcb0e0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGJvb2slMjBncm91cCUyMGZyZWUlMjB0byUyMHVzZXxlbnwwfDB8MHx8fDI",
-        brief_desc="Friendly reading group.",
+        brief_desc="Friendly reading group",
         contact_name="Catherine",
         contact_number="07010 110 110",
         full_desc="Our friendly reading group meets once a month on a Monday evening to discuss our latest book. Feel free to come along every month, or to skip a month if you don't fancy the book we've chosen! Call Catherine for more info!",
@@ -90,8 +90,8 @@ with app.app_context():
 
     italian_conversation = GroupModel(
         name="Italian Conversation Group",
-        image="https://images.unsplash.com/photo-1552832230-c0197dd311b5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cm9tZSUyMGZyZWUlMjB0byUyMHVzZXxlbnwwfDB8MHx8fDI",
-        brief_desc="Join other learners of Italian for conversation practice.",
+        image="https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA",
+        brief_desc="Practise Italian with us",
         contact_name="Wendy",
         contact_number="07020 220 220",
         full_desc="Every other Thursday we meet to practise our Italian. We members at all levels, from beginner to advanced - all are welcome at our supportive group.",
@@ -102,7 +102,7 @@ with app.app_context():
     golden_gardeners = GroupModel(
         name="The Golden Gardeners",
         image="https://images.unsplash.com/photo-1584479898061-15742e14f50d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGdhcmRlbmluZyUyMGZyZWUlMjB0byUyMHVzZXxlbnwwfDB8MHx8fDI",
-        brief_desc="Enthusiastic gardening group!",
+        brief_desc="Enthusiastic gardening group",
         contact_name="George",
         contact_number="07030 330 330",
         full_desc="At Golden Gardeners we help each other to look after our gardens. As the saying goes, many hands make light work. And there's plenty of time for tea cake, too!",
@@ -113,7 +113,7 @@ with app.app_context():
     drama_queens = GroupModel(
         name="The Drama Queens",
         image="https://images.unsplash.com/photo-1516307365426-bea591f05011?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dGhlYXRyZSUyMGZyZWUlMjB0byUyMHVzZXxlbnwwfDB8MHx8fDI",
-        brief_desc="A local group of theatregoers.",
+        brief_desc="A local group of theatregoers",
         contact_name="Catherine",
         contact_number="07010 110 110",
         full_desc="The Drama Queens organise theatre trips to the city centre, and sometimes further afield if there's something great on! Join us as little or as often as you'd like.",
@@ -124,7 +124,7 @@ with app.app_context():
     harborne_social_group = GroupModel(
         name="Harborne Social Group",
         image="https://images.unsplash.com/photo-1606509036992-4399d5c5afe4?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29mZmVlJTIwY3Vwc3xlbnwwfDB8MHx8fDI",
-        brief_desc="Informal social group.",
+        brief_desc="Informal social group",
         contact_name="Wendy",
         contact_number="07020 220 220",
         full_desc="We are a (very!) informal social group and we meet on the first Sunday of every month for coffee and/or brunch on the high street. We have members of all ages. Give Wendy a call if you'd like to come along!",
