14bdcc6cc15a48446f71e34af9d48bcfb1068329
Catherine Brett <catherinejanebrett@icloud.com>
2024-04-19 10:49:43 +0100

    Ready for deployment

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 from config.environment import db_URI
+
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = db_URI
+
+CORS(app)
 
 db = SQLAlchemy(app)
 
--- config/environment.py
+++ config/environment.py
@@ -1,2 +1,7 @@
-db_URI = "postgresql://localhost:5432/groups_db"
-SECRET = "umbrellagrandioseboatlimelibrary"
+import os
+
+db_URI = os.getenv("DATABASE_URL", "postgresql://localhost:5432/groups_db")
+SECRET = os.getenv("SECRET", "umbrellagrandioseboatlimelibrary")
+
+if db_URI.startswith("postgres://"):
+    db_URI = db_URI.replace("postgres://", "postgresql://", 1)
